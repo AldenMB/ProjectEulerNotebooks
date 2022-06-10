4b06a328417dfaf8b489b48e40483c468463c39c
Alden Bradford <aldenbradford@gmail.com>
2022-06-10 03:15:40 -0400

    scheduling utilities

---
+++ euler_binder/__main__.py
@@ -0,0 +1,5 @@
+if __name__ == "__main__":
+    from .fetch import fetch_all
+    from .schedule import schedule
+    fetch_all()
+    schedule()
---
+++ euler_binder/schedule.py
@@ -0,0 +1,24 @@
+import datetime
+from pathlib import Path
+import re
+
+from .fetch import upcoming
+
+
+record = Path('./.github/workflows/fetch.yml')
+
+
+def to_cron(dt):
+    return f"    - cron: '{dt.minute} {dt.hour} {dt.day} {dt.month} *'"
+
+def schedule():
+    stamps = (line.split('##')[1] for line in upcoming().splitlines())
+    dts = (datetime.datetime.fromtimestamp(int(stamp)) for stamp in stamps)
+    margin = datetime.timedelta(minutes = 5)
+    new_crons = '\n' + '\n'.join(to_cron(dt+margin) for dt in dts) + '\n'
+    
+    with open(record, 'r') as f:
+        new_file = re.sub(r"(\n?    - cron: '.*?'\n)+", new_crons, f.read())
+
+    with open(record, 'w') as f:
+        f.write(new_file)
