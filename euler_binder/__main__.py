da85f79752d647c676e1f03a0558a9a781bd5bd7
Alden Bradford <aldenbradford@gmail.com>
2022-06-19 12:55:30 -0400

    remove cache from trial http requests

--- euler_binder/__main__.py
+++ euler_binder/__main__.py
@@ -1,6 +1,6 @@
 trial_urls = [
     "https://google.com",
-    "https://amazon.com",
+    "https://httpbin.org",
     "https://projecteuler.net",
     "https://projecteuler.net/problem=1",
     "https://projecteuler.net/minimal=1",
@@ -9,13 +9,11 @@ trial_urls = [
 ]
 
 def try_fetching_stuff():
-    from .fetch import session
     from requests import get
     for url in trial_urls:
-        for interface in session.get, get:
-            res = interface(url)
-            req = res.request
-            print(f"url {url} gave response {res.status_code} with reason {res.reason}. The request was for {req.url}, sending content {req.body} and headers {req.headers}")
+        res = get(url)
+        req = res.request
+        print(f"url {url} gave response {res.status_code} with reason {res.reason}. The request was for {req.url}, sending content {req.body} and headers {req.headers}")
 
 
 if __name__ == "__main__":
